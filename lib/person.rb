2186c4260b339deac53866fe13f6669b9fd59b1b
epittab <ep7m@me.com>
2020-04-22 12:57:46 -0400

    questions 11-20, 1 fail

--- lib/person.rb
+++ lib/person.rb
@@ -1,8 +1,8 @@
 # your code goes here
 class Person
-    attr_reader :name, :happiness
+    attr_reader :name, :happiness,:hygiene 
     attr_writer 
-    attr_accessor :bank_account, :hygiene 
+    attr_accessor :bank_account
     def initialize(name)
         @name = name
         @bank_account = 25
@@ -19,5 +19,39 @@ class Person
         @happiness = happiness
     end
 
+    def hygiene=(hygiene)
+        if hygiene >= 10
+            hygiene = 10
+        elsif  hygiene <=0 
+            hygiene = 0
+        end
+        @hygiene = hygiene
+    end
+
+    def happy?
+        if @happiness > 7
+            return true
+        else
+            return false
+        end
+    end
+
+    def clean?
+        if @hygiene > 7
+            return true
+        else
+            return false
+        end
+    end
+
+    def get_paid(salary)
+        @bank_account += salary
+        return "all about the benjamins"
+    end
+
+    def take_bath()
+        @hygiene += 4
+        return "♪ Rub-a-dub just relaxing in the tub ♫"
+    end
 
 end
