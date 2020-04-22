7972233e89d886238979fcdb51cd267059107b85
Alexander_Gautherin <alexander.gautherin@uconn.edu>
2020-04-22 12:44:44 -0400

    12 examples, 1 fail

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,23 @@
 # your code goes here
+class Person
+    attr_reader :name, :happiness
+    attr_writer 
+    attr_accessor :bank_account, :hygiene 
+    def initialize(name)
+        @name = name
+        @bank_account = 25
+        @happiness = 8
+        @hygiene = 8
+    end
+
+    def happiness=(happiness)
+        if happiness >= 10
+            happiness = 10
+        elsif  happiness <=0 
+            happiness = 0
+        end
+        @happiness = happiness
+    end
+
+
+end
