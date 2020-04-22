1c5cc0cdeba60a10fedeac2ec428b60602f18920
epittab <ep7m@me.com>
2020-04-22 14:17:25 -0400

    45 examples, 1 failure.

--- lib/person.rb
+++ lib/person.rb
@@ -67,4 +67,17 @@ class Person
         
     end
 
+    def start_conversation(person, topic)
+        if topic == 'politics'
+            person.happiness -= 2
+            self.happiness -= 2
+            return "blah blah partisan blah lobbyist"
+        elsif topic == 'weather'
+            person.happiness += 1
+            self.happiness += 1
+            return "blah blah sun blah rain"
+        end
+    end
+
+
 end
